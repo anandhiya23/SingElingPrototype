837f93e5cdc43aa81f444479484fabcc7942f8e4
HalizaSyafa27 <oktavianihaliza@gmail.com>
2024-10-23 14:57:45 +0700

    feat: animationView

---
+++ SingElingPrototype/Model/GameModel.swift
@@ -0,0 +1,8 @@
+//
+//  GameModel.swift
+//  SingElingPrototype
+//
+//  Created by Haliza Syafa Oktaviani on 22/10/24.
+//
+
+import Foundation
--- SingElingPrototype/ViewModel/GameManager.swift
+++ SingElingPrototype/ViewModel/GameManager.swift
@@ -11,62 +11,62 @@ import os
 import SwiftUI
 
 //MARK: - NETWORK MODELS
-enum SendableGameDataType: Codable{
-    case gameCommand, gameState
-}
-enum GameCommandType: Codable{
-    case assignPID, cardPosShiftRight, cardPosShiftLeft, hideOthersCards, showOthersCards, invokeNextTurn
-}
-struct GameCommand: Codable{
-    var command: GameCommandType
-    var intData: Int? = nil
-    var stringData: String? = nil
-    var boolData: Bool?  = nil
-    
-    init(_ command: GameCommandType, intData: Int? = nil, stringData: String? = nil, boolData: Bool? = nil) {
-        self.command = command
-        self.intData = intData
-        self.stringData = stringData
-        self.boolData = boolData
-    }
-}
-struct SendableGameData: Codable{
-    var type: SendableGameDataType
-    var gameCommand: GameCommand?
-    var gameState: GameState?
-    var sender_PID: Int
-}
-enum ConnectivityType{
-    case host, guest, unknown
-}
+//enum SendableGameDataType: Codable{
+//    case gameCommand, gameState
+//}
+//enum GameCommandType: Codable{
+//    case assignPID, cardPosShiftRight, cardPosShiftLeft, hideOthersCards, showOthersCards, invokeNextTurn
+//}
+//struct GameCommand: Codable{
+//    var command: GameCommandType
+//    var intData: Int? = nil
+//    var stringData: String? = nil
+//    var boolData: Bool?  = nil
+//    
+//    init(_ command: GameCommandType, intData: Int? = nil, stringData: String? = nil, boolData: Bool? = nil) {
+//        self.command = command
+//        self.intData = intData
+//        self.stringData = stringData
+//        self.boolData = boolData
+//    }
+//}
+//struct SendableGameData: Codable{
+//    var type: SendableGameDataType
+//    var gameCommand: GameCommand?
+//    var gameState: GameState?
+//    var sender_PID: Int
+//}
+//enum ConnectivityType{
+//    case host, guest, unknown
+//}
 
 //MARK: - GAME MODELS
-struct PlayingCard{
-    var text: String
-    var icon: String = ""
-    var indexNum: Int = 30
-}
-struct Player: Codable, Hashable{
-    var name: String = "PLACEHOLDER_NAME"
-    var point: Int = 0
-    var cardPos: Int = 1
-    var playingCards_CID: [Int] = []
-}
-struct GameState: Codable{
-    var availablePlayingCards_CID: [Int] = []
-    var isPlaying: Bool = false
-    var players: [Player] = []
-    var othersCardsHidden = false
-    var triggerGuesserCardShift = false
-    var readerCard_CID = 0
-    var winner_PID: Int? = nil
-    
-    //READER
-    var reader_PID: Int =  0
-    
-    //GUESSER
-    var guesser_PID: Int = 1
-}
+//struct PlayingCard{
+//    var text: String
+//    var icon: String = ""
+//    var indexNum: Int = 30
+//}
+//struct Player: Codable, Hashable{
+//    var name: String = "PLACEHOLDER_NAME"
+//    var point: Int = 0
+//    var cardPos: Int = 1
+//    var playingCards_CID: [Int] = []
+//}
+//struct GameState: Codable{
+//    var availablePlayingCards_CID: [Int] = []
+//    var isPlaying: Bool = false
+//    var players: [Player] = []
+//    var othersCardsHidden = false
+//    var triggerGuesserCardShift = false
+//    var readerCard_CID = 0
+//    var winner_PID: Int? = nil
+//    
+//    //READER
+//    var reader_PID: Int =  0
+//    
+//    //GUESSER
+//    var guesser_PID: Int = 1
+//}
 
 //MARK: - MAIN GAME MANAGER
 class GameManager: NSObject, ObservableObject{
