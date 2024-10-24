2cd27d433fcc9bd0f302d82a077220e69560eed6
Lazuardhi Imani Ahfar <115421179+LilZuar@users.noreply.github.com>
2024-10-24 14:32:18 +0700

    component jempol

---
+++ SingElingPrototype/View/Component/Jempol.swift
@@ -0,0 +1,20 @@
+//
+//  Jempol.swift
+//  SingElingPrototype
+//
+//  Created by Lazuardhi Imani Ahfar on 24/10/24.
+//
+
+import SwiftUI
+
+struct Jempol: View {
+    var body: some View {
+        Image("Jempol")
+            .resizable()
+            .frame(width: 115, height: 167)
+    }
+}
+
+#Preview {
+    Jempol()
+}
