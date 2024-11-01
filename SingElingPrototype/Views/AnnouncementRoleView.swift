1f94bdc41b5d1755276af4acdfca392f7a83d11e
Lazuardhi Imani Ahfar <115421179+LilZuar@users.noreply.github.com>
2024-11-01 15:26:03 +0700

    feat: adding announcement role timer

---
+++ SingElingPrototype/Views/AnnouncementRoleView.swift
@@ -0,0 +1,18 @@
+//
+//  AnnouncementRoleView.swift
+//  SingElingPrototype
+//
+//  Created by Lazuardhi Imani Ahfar on 01/11/24.
+//
+
+import SwiftUI
+
+struct AnnouncementRoleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnnouncementRoleView()
+}
