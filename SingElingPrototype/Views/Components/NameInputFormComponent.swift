6c95fb5d6e8311f30dfb6394c0f204b1c4b5e106
HalizaSyafa27 <oktavianihaliza@gmail.com>
2024-11-01 15:01:27 +0700

    feat: add MainViewHaliza
    add MainViewHaliza to test

--- SingElingPrototype/Views/Components/NameInputFormComponent.swift
+++ SingElingPrototype/Views/Components/NameInputFormComponent.swift
@@ -70,8 +70,9 @@ struct NameInputFormComponent: View {
                 .offset(y: -25)
             }
         }
-        .padding()
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        //apus ini
+//        .padding()
+//        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
