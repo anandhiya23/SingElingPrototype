d535473b140ab4a41ddac80961857ea2c934b299
Bintang Anandhiya <bintanganandhiya23@gmail.com>
2024-11-01 16:05:31 +0700

    Full Giliran Page

--- SingElingPrototype/Views/Components/BintangTestDragDrop2.swift
+++ SingElingPrototype/Views/Components/BintangTestDragDrop2.swift
@@ -31,6 +31,12 @@ struct DragListItemView: View {
     }
 }
 
+extension Comparable {
+    func clamped(to limits: ClosedRange<Self>) -> Self {
+        return min(max(self, limits.lowerBound), limits.upperBound)
+    }
+}
+
 struct TempPlayer {
     var name: String
     var color: Color
@@ -45,98 +51,135 @@ struct BintangDragDropView2: View {
     @State var tempGetObject: TempPlayer = TempPlayer(name: "", color: Color.red)
     
     @State var players: [TempPlayer] = [
-        TempPlayer(name: "Bintang", color: .singPink),
-        TempPlayer(name: "Azuar", color: .singGreen),
-        TempPlayer(name: "Haliza", color: .orange),
-        TempPlayer(name: "Reyhan", color: .blue),
-        TempPlayer(name: "Kale", color: .red)
+        TempPlayer(name: "Bintang", color: .singElingLC50),
+        TempPlayer(name: "Azuar", color: .singElingDSB50),
+        TempPlayer(name: "Reyhan", color: .singElingSB50),
+        TempPlayer(name: "Kale", color: .singElingZ50)
         ]
 
     var body: some View {
-        Text("Atur giliran\nMainmu, Yuk!")
-            .font(.custom("skrapbook", size: 40))
-        RoundedRectangle(cornerRadius: 10)
-            .fill(.backgroundStatement)
-            .frame(width: 340, height: 80)
-            .overlay {
-                HStack{
-                    Image(systemName: "questionmark.circle.fill")
-                        .font(.system(size: 40))
-                    Text("Susun urutan giliran main sesuai kesepakatan dengan geser dan lepas, santai aja!")
-                        .font(.custom("skrapbook", size: 18))
-                        .multilineTextAlignment(.center)
-                }
-                .padding(.horizontal,10)
-            }
-        
-        HStack{
+        ZStack{
+            Color.singElingDS50
             VStack{
-                ForEach(1..<6, id: \.self){ index in
-                    RoundedRectangle(cornerRadius: 10)
-                        .fill(Color.singCardText)
-                        .strokeBorder(Color.black,style: StrokeStyle(lineWidth: 5))
-                        .frame(width: 80, height: 80)
-                        .overlay {
-                            Text("\(index)")
-                                .font(.custom("skrapbook", size: 40))
+                Spacer()
+                Text("Atur giliran\nMainmu, Yuk!")
+                    .font(.custom("skrapbook", size: 40))
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(Color.singElingDS30)
+                    .frame(width: 340, height: 80)
+                    .overlay {
+                        HStack{
+                            Image(systemName: "questionmark.circle.fill")
+                                .font(.system(size: 40))
+                            Text("Susun urutan giliran main sesuai kesepakatan dengan geser dan lepas, santai aja!")
+                                .font(.custom("skrapbook", size: 18))
+                                .multilineTextAlignment(.center)
                         }
-                    
-                }
-            }
-            .padding(.top, 8)
-            VStack(spacing: 8) {
-                ForEach(players.indices, id: \.self) { index in
-                    let curPlayerName = players[index].name
-                    DragListItemView(curPlayerName, players[index].color)
-                        .padding(.top, lastItemPos == index && isDragging ? 88 : 0)
-                        .padding(.bottom, lastItemPos - 1 == index && isDragging && players.count == lastItemPos ? 88 : 0)
-                        .simultaneousGesture(
-                            LongPressGesture(minimumDuration: 0.05)
-                                .onEnded { success in
-                                    if success{
-                                        isDragging = true
-                                        draggedIndex = index
-                                        lastItemPos = index
-                                        tempGetObject = players.remove(at: lastItemPos)
+                        .padding(.horizontal,10)
+                    }
+                
+                HStack{
+                    VStack{
+                        ForEach(1..<5, id: \.self){ index in
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(Color.singElingSB10)
+                                .strokeBorder(Color.black,style: StrokeStyle(lineWidth: 5))
+                                .frame(width: 80, height: 80)
+                                .overlay {
+                                    Text("\(index)")
+                                        .font(.custom("skrapbook", size: 40))
+                                }
+                            
+                        }
+                    }
+                    .padding(.top, 8)
+                    VStack(spacing: 8) {
+                        ForEach(players.indices, id: \.self) { index in
+                            let curPlayerName = players[index].name
+                            DragListItemView(curPlayerName, players[index].color)
+                                .padding(.top, lastItemPos == index && isDragging ? 88 : 0)
+                                .padding(.bottom, lastItemPos - 1 == index && isDragging && players.count == lastItemPos ? 88 : 0)
+                                .simultaneousGesture(
+                                    LongPressGesture(minimumDuration: 0.05)
+                                        .onEnded { success in
+                                            if success{
+                                                isDragging = true
+                                                draggedIndex = index
+                                                lastItemPos = index
+                                                tempGetObject = players.remove(at: lastItemPos)
+                                            }
+                                        }
+                                )
+                            //                    .animation(.spring(), value: isDragging)
+                        }
+                    }
+                    .simultaneousGesture(DragGesture(minimumDistance: 0)
+                        .onChanged { dragValue in
+                            if isDragging{
+                                dragOffset = dragValue.translation.height
+                                indexOffset = Int(dragOffset / 88.0)
+                                //                    print("\(draggedIndex) + \(indexOffset)")
+                                let dropIndex = (draggedIndex + indexOffset).clamped(to: 0...players.count)
+                                if dropIndex != lastItemPos {
+                                    withAnimation(.bouncy.speed(2)){
+                                        lastItemPos = dropIndex
                                     }
                                 }
-                        )
-    //                    .animation(.spring(), value: isDragging)
-                }
-            }
-            .simultaneousGesture(DragGesture(minimumDistance: 0)
-                .onChanged { dragValue in
-                    if isDragging{
-                        dragOffset = dragValue.translation.height
-                        indexOffset = Int(dragOffset / 88.0)
-    //                    print("\(draggedIndex) + \(indexOffset)")
-                        if draggedIndex + indexOffset != lastItemPos {
-                            withAnimation(.bouncy.speed(2)){
-                                lastItemPos = draggedIndex + indexOffset
                             }
                         }
-                    }
-                }
-                .onEnded { val in
-                    if isDragging{
-                        players.insert(tempGetObject, at: lastItemPos)
-                        isDragging = false
-                    }
-                }
-            )
-            .padding(.top, 8)
-            .overlay(alignment: .top) {
-                if isDragging {
-                    DragListItemView(tempGetObject.name, tempGetObject.color)
-                        .scaleEffect(1.07)
-                        .background{
-                            RoundedRectangle(cornerRadius: 10)
-                                .shadow(radius: 8, y: 5)
+                        .onEnded { val in
+                            if isDragging{
+                                players.insert(tempGetObject, at: lastItemPos)
+                                isDragging = false
+                            }
                         }
-                        .position(x: 125, y: CGFloat((draggedIndex + 1) * 88) - 40.0 + dragOffset)
+                    )
+                    .padding(.top, 8)
+                    .overlay(alignment: .top) {
+                        if isDragging {
+                            DragListItemView(tempGetObject.name, tempGetObject.color)
+                                .scaleEffect(1.07)
+                                .background{
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .shadow(radius: 8, y: 5)
+                                }
+                                .position(x: 125, y: CGFloat((draggedIndex + 1) * 88) - 40.0 + dragOffset)
+                        }
+                    }
                 }
+                Spacer()
+                ZStack{
+                    Color.singElingLC50
+                    Button {
+                        print("blahaha")
+                    } label: {
+                        RoundedRectangle(cornerRadius: 12)
+                            .fill(.singElingDS10)
+                            .strokeBorder(Color.black,style: StrokeStyle(lineWidth: 5))
+                            .frame(width: 180, height: 80)
+                            .overlay {
+                                HStack{
+                                    Image(systemName: "hand.thumbsup.fill")
+                                        .foregroundStyle(.black)
+                                        .font(.system(size: 30))
+                                    Text("Lanjut!")
+                                        .foregroundStyle(.black)
+                                        .font(.custom("skrapbook", size: 30))
+                                }
+                                
+                            }
+                            .background {
+                                RoundedRectangle(cornerRadius: 12)
+                                    .fill(.singElingDS70)
+                                    .strokeBorder(Color.black,style: StrokeStyle(lineWidth: 5))
+                                    .padding(.bottom,-10)
+                            }
+                    }
+                }
+                .frame(height: 140)
             }
         }
+        .ignoresSafeArea()
     }
 }
 
