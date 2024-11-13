47ece0244d1f133afae944dc77c5c14885757c13
HalizaSyafa27 <oktavianihaliza@gmail.com>
2024-11-13 14:29:19 +0700

    feat: modify GameManager, modify CountDownView, modify AnnouncementRoleView, modify GamePlayTutorial

--- SingElingPrototype/Views/GamePlayTutorial.swift
+++ SingElingPrototype/Views/GamePlayTutorial.swift
@@ -1,15 +1,10 @@
 //
-<<<<<<< Updated upstream
+
 //  GameViewTutorial.swift
 //  SingElingPrototype
 //
 //  Created by Haliza Syafa Oktaviani on 12/11/24.
-=======
-//  GamePlayTutorial.swift
-//  SingElingPrototype
-//
-//  Created by Haliza Syafa Oktaviani on 11/11/24.
->>>>>>> Stashed changes
+
 //
 
 import SwiftUI
@@ -18,10 +13,8 @@ struct GamePlayTutorial: View {
     @EnvironmentObject var gameManager: GameManager
     @State private var vw: CGFloat = 0
     @State private var vh: CGFloat = 0
-<<<<<<< Updated upstream
-=======
+
     @Binding var curView: Int
->>>>>>> Stashed changes
     
     var body: some View {
         GeometryReader { geom in
@@ -33,8 +26,8 @@ struct GamePlayTutorial: View {
                     Spacer(minLength: 0)
                     RoleTitleView(role: "Penebak")
                     Spacer(minLength: 20)
-<<<<<<< Updated upstream
-                    CenterCardsView()
+
+//                    CenterCardsView()
                     Spacer(minLength: 40)
                     LockButtonView()
                 }
@@ -45,28 +38,26 @@ struct GamePlayTutorial: View {
                 vh = geom.size.height
             }
             .ignoresSafeArea()
-=======
+
                     Spacer(minLength: 40)
                     ButtonComponent(width: 164, height: 64, action: {
                         print("Button tapped")
                     }, buttonModel: ButtonModel(button: .kunci))
                 }
-                .frame(width: geom.size.width, height: geom.size.height) // Set frame to screen size
+//                .frame(width: geom.size.width, height: geom.size.height) // Set frame to screen size
             }
-            .frame(width: geom.size.width, height: geom.size.height)  // Center ZStack in GeometryReader
+//            .frame(width: geom.size.width, height: geom.size.height)  // Center ZStack in GeometryReader
             
->>>>>>> Stashed changes
         }
-    }
-}
+
 
 struct BackgroundView: View {
     var body: some View {
-<<<<<<< Updated upstream
+
         Image("DefaultBackground") // Replace with dynamic background if needed
-=======
+
         Image("Tikar Abu")
->>>>>>> Stashed changes
+
             .resizable()
             .scaledToFill()
             .ignoresSafeArea()
@@ -76,7 +67,7 @@ struct BackgroundView: View {
 struct HeaderView: View {
     var body: some View {
         HStack {
-<<<<<<< Updated upstream
+
             Button(action: {
                 // Action for "KELUAR" button
             }) {
@@ -96,9 +87,9 @@ struct HeaderView: View {
                 .padding()
                 .background(Color.yellow)
                 .cornerRadius(8)
-=======
+
             Spacer()
->>>>>>> Stashed changes
+
         }
         .padding()
         .background(Color.singElingDS50)
@@ -119,39 +110,39 @@ struct RoleTitleView: View {
     }
 }
 
-<<<<<<< Updated upstream
-struct CenterCardsView: View {
-    @EnvironmentObject var gameManager: GameManager
-    @State private var vw: CGFloat = UIScreen.main.bounds.width
-    @State private var vh: CGFloat = UIScreen.main.bounds.height
-    
-    var body: some View {
-        ZStack {
-            HStack(spacing: 16) {
-                ForEach(0..<gameManager.guesserCards.count, id: \.self) { curCardIdIndice in
-                    let curCardId = gameManager.guesserCards[curCardIdIndice]
-                    let tempCard: PlayingCard = gameManager.playingCards[curCardId]
-                    
-                    CardComponent(
-                        width: 147,
-                        text: tempCard.text,
-                        indexNum: tempCard.indexNum,
-                        backgroundImage: gameManager.getBackground(for: tempCard.indexNum)
-                    )
-                    .padding(.leading, curCardIdIndice == gameManager.guesserCardPos ? 20 : (curCardIdIndice == gameManager.guesserCardPos + 1 ? 60 : -80))
-                }
-            }
-            .padding(.leading, (vw / 2) - CGFloat(gameManager.guesserCardPos * 85))
-            .frame(width: vw, alignment: .leading)
-            
-            // Finger indicator between cards
-            Jempol(width: 135, height: 196)
-                .rotationEffect(Angle(degrees: 180))
-                .frame(width: 70, height: 60)
-                .position(x: vw / 1.9, y: 50) // Adjusted Y-position for placement
-        }
-    }
-}
+
+//struct CenterCardsView: View {
+//    @EnvironmentObject var gameManager: GameManager
+//    @State private var vw: CGFloat = UIScreen.main.bounds.width
+//    @State private var vh: CGFloat = UIScreen.main.bounds.height
+//    
+//    var body: some View {
+//        ZStack {
+//            HStack(spacing: 16) {
+//                ForEach(0..<gameManager.guesserCards.count, id: \.self) { curCardIdIndice in
+//                    let curCardId = gameManager.guesserCards[curCardIdIndice]
+//                    let tempCard: PlayingCard = gameManager.playingCards[curCardId]
+//                    
+//                    CardComponent(
+//                        width: 147,
+//                        text: tempCard.text,
+//                        indexNum: tempCard.indexNum,
+//                        backgroundImage: gameManager.getBackground(for: tempCard.indexNum)
+//                    )
+//                    .padding(.leading, curCardIdIndice == gameManager.guesserCardPos ? 20 : (curCardIdIndice == gameManager.guesserCardPos + 1 ? 60 : -80))
+//                }
+//            }
+//            .padding(.leading, (vw / 2) - CGFloat(gameManager.guesserCardPos * 85))
+//            .frame(width: vw, alignment: .leading)
+//            
+//            // Finger indicator between cards
+//            Jempol(width: 135, height: 196)
+//                .rotationEffect(Angle(degrees: 180))
+//                .frame(width: 70, height: 60)
+//                .position(x: vw / 1.9, y: 50) // Adjusted Y-position for placement
+//        }
+//    }
+//}
 
 struct LockButtonView: View {
     var body: some View {
@@ -172,13 +163,13 @@ struct LockButtonView: View {
     }
 }
 
-struct GamePlayTutorial_Previews: PreviewProvider {
-    static var previews: some View {
-        GamePlayTutorial()
-            .environmentObject(GameManager(username: "hhh")) // Provide GameManager for preview
-    }
-}
-=======
+//struct GamePlayTutorial_Previews: PreviewProvider {
+//    static var previews: some View {
+//        GamePlayTutorial()
+//            .environmentObject(GameManager(username: "hhh")) // Provide GameManager for preview
+//    }
+//}
+
 //struct CenterCardsView: View {
 //    @EnvironmentObject var gameManager: GameManager
 //    @State private var vw: CGFloat = UIScreen.main.bounds.width
@@ -210,12 +201,5 @@ struct GamePlayTutorial_Previews: PreviewProvider {
 //    }
 //}
 
-struct GamePlayTutorial_Previews: PreviewProvider {
-    static var previews: some View {
-        GamePlayTutorial(curView: .constant(0))
-            .environmentObject(GameManager(username: "hhh"))
-    }
-}
 
 
->>>>>>> Stashed changes
